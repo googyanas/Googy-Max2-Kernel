c4189032216b3234c5f29dc911364aa56cda46b3
googyanas <googyanas@gmail.com>
2014-02-08 20:27:54 +0000

    fix TriangleAway (Derteufel)

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1196,13 +1196,13 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 	/*
 	 * Ensure eMMC boot partition Write protection
 	 */
-	err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
+/*	err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
 			EXT_CSD_BOOT_WP,
 			EXT_CSD_BOOT_WP_PERM_WP_DIS | EXT_CSD_BOOT_WP_PWR_WP_EN,
 			card->ext_csd.part_time);
 	if (err && err != -EBADMSG)
 		goto free_card;
-
+*/
 	/*
 	 * Ensure eMMC boot config is protected.
 	 */
