836e201027d855e897fb853eaa1997e5d1192643
googyanas <googyanas@gmail.com>
2014-08-13 17:52:15 +0100

    smdk4412: update mali driver
    from GT-N7100_SEA_KK_Opensource

--- drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
+++ drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
@@ -463,6 +463,9 @@ void mali_clk_set_rate(unsigned int clk, unsigned int mhz)
 	
 	if (bis_vpll)
 	{
+		/* in Pega-prime, vpll_src_clock means ext_xtal_clock!! */
+		clk_set_parent(sclk_vpll_clock, vpll_src_clock);
+
 		clk_set_rate(fout_vpll_clock, (unsigned int)clk * GPU_MHZ);
 		clk_set_parent(vpll_src_clock, ext_xtal_clock);
 		clk_set_parent(sclk_vpll_clock, fout_vpll_clock);
